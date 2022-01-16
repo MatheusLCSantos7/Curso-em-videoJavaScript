1b0639cf26f6b252aecfea075bfb3584404bf828
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-01-15 21:23:48 -0300

    CONCEITOS DE condições

---
+++ Aula 07/ex008.js
@@ -0,0 +1,9 @@
+var vel = 50.5
+
+console.log(`A velocidade do seu carro é ${vel}km por hora`)
+
+if (vel >60)   //condição simples  
+{
+     console.log(`Você ultrapassou a velocidade permitida`)
+}
+console.log(`digrija sempre usando cinto`)
---
+++ Aula 07/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'eua'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'brasil') 
+{
+    console.log('Voce é brasileiro')
+}
+else(pais == 'eua')
+{
+    console.log('Você é estrangeiro')
+}
