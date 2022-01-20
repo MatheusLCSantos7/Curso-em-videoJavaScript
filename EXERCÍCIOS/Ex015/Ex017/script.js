be90a6741a64714527c110abeb7601a0d5841c93
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-01-20 16:18:04 -0300

    Não esta concluido

--- EXERCÍCIOS/Ex015/Ex017/script.js
+++ EXERCÍCIOS/Ex015/Ex017/script.js
@@ -1,1 +1,20 @@
-alert('Ola!')
+function clicou()
+{
+var tn1 = window.document.getElementById('num')
+var res = window.document.getElementById('res')
+var n1 = Number(tn1.value)
+
+if (tn1.value.length == 0)
+{
+    res.innerHTML = 'Impossível contar'
+    window.alert('[ERRO] Digite um número')
+}else 
+{
+    for (let c = 0; c <= 9; c +=n1)
+    {
+    let n1 = n1 * c
+    res.innerHTML = `${n1}`
+
+    }
+}
+} 
