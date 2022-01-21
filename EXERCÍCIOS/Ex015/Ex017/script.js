12df94231c16cfb629cf52a160d525548348b29c
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-01-20 21:44:30 -0300

    EXERCÍCIO COMPLETO

--- EXERCÍCIOS/Ex015/Ex017/script.js
+++ EXERCÍCIOS/Ex015/Ex017/script.js
@@ -1,21 +1,22 @@
 function clicou()
 {
-var tn1 = window.document.getElementById('num')
+var tn1 = window.document.getElementById('txtn')
 var res = window.document.getElementById('res')
-var n1 = Number(tn1.value)
-
 if (tn1.value.length == 0)
 {
     res.innerHTML = 'Impossível contar'
     window.alert('[ERRO] Digite um número')
 } else {
-
-for(var c = 0;c <=10;c++)
-{
-var padrao = document.createElement('li')
-padrao.appendChild(document.createTextNode((n1.value) * i))
-document.querySelector('ul').appendChild('li')
-
-}
+    var n1 = Number(tn1.value)
+    var c = 1
+    res.innerHTML = ''
+   while (c <=10) 
+   {
+    var item = document.createElement('option')
+    item.text = `${n1} x ${c} = ${n1*c}`
+    item.value = `tab${c}`
+    res.appendChild(item)
+    c++     
+   }
 }
 } 
