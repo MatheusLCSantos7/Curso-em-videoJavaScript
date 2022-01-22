af9cd736022f91039ec3aa66ca0f8f807081eb8f
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-01-21 21:35:48 -0300

    Primeiro dia de tentativa do desafio

---
+++ EXERCÍCIOS/último desafio/script.js
@@ -0,0 +1,15 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let vetores = []
+
+function adicionar()
+{
+    if(isnumero(num.value) && !inlista(num.value))
+    {
+
+    }  
+    else{window.alert('VALOR INVALIDO OU JA ENCONTRADO NA LISTA')}
+
+    //parte 7 exercicios recomeçar vidos
+}
